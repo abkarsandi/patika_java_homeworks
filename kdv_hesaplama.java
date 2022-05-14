fb1e9a580ecf860892d772720f9418c70e7ee024
Aprencher <aprencher@gmail.com>
2022-05-15 02:23:20 +0300

    ürün değeri girildiğinde kdvli tutarı veren program.

---
+++ kdv_hesaplama.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class kdv_hesaplama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double value = 1.18 * input.nextDouble();
+        System.out.println(value);
+    }
+}
